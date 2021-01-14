2a3af4bea7968b257f51110d9db15b7ebb454ffc
Rayane LAHRACHE <rlahrache@iutbayonne.univ-pau.fr>
2021-01-14 20:11:14 +0100

    Premier peuplement BD

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,14 +4,21 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Entreprise;
+use App\Entity\Formation;
+use App\Entity\Stage;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        $entrepriseTest= new Entreprise();
+        $entrepriseTest->setNom("Safran");
+        $entrepriseTest->setAdresse("Avenue du 1er Mai");
+        $entrepriseTest->setMilieu("Aéronotique");
+        $entrepriseTest->setTelephone("05 59 74 40 00");
+        $entrepriseTest->setPhoto("https://www.safran-helicopter-engines.com/sites/turbomeca/files/styles/landscape_large/public/thumbnails/image/usine_tarnos_01.jpg?itok=-abUxRz_");
+        $manager->persist($entrepriseTest);
         $manager->flush();
     }
 }
